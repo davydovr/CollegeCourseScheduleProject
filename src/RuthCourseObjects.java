817a9b4a839e65091e1dd50ce05365cda1c83325
Ruth Davydov <ruthdavydov@gmail.com>
2018-11-21 15:08:25 -0500

    exhanced GUI to dynamically resize table columns to fit all content, added enter key event listener (so program responds to both clicking Go button and pressing enter)

--- src/RuthCourseObjects.java
+++ src/RuthCourseObjects.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 public class RuthCourseObjects {
@@ -16,6 +17,68 @@ public class RuthCourseObjects {
 		ruthCourses.add( new Course(13394, "MATN261", "Statistics For Soc Sci", "Siegel Stephanie", 01, 3));
 		ruthCourses.add( new Course(13138, "PSYN101", "Introduction To Psychology", "Steinman David", 01 , 3));
 		ruthCourses.add( new Course(11111, "HISN220", "Survey Mod History I", "Appleseed Johnny", 03, 3));
+		ruthCourses.add( new Course(11261, "BION101", "Principles Of Biology I", "Borger Rivka", 02 ,4));
+		ruthCourses.add( new Course(13585, "BION400", "Neuroscience", "Levine Alan", 02 ,3));
+		ruthCourses.add( new Course(11262, "BION101L", "Principles Of Biology I Lab", "Borger Rivka", 04 ,0));
+		ruthCourses.add( new Course(12671, "EBMN101", "Principles Of Management", "Felder Simcha", 04 ,3));
+		ruthCourses.add( new Course(10618, "HISN221", "Survey Mod History II", "TBA", 04 ,3));
+		ruthCourses.add( new Course(12756, "LLEN102", "English Composition II", "Epstein Janey", 04 ,3));
+		ruthCourses.add( new Course(13139, "PSYN205", "Psyc Of Motivation", "Litwin Elissa", 04 ,3));
+		ruthCourses.add( new Course(13141, "PSYN325", "Drugs And Behavior", "Steinman David", 04 ,3));
+		ruthCourses.add( new Course(13381, "SASN103", "Introduction To Sociology", "Ratti Deborah", 04 ,3));
+		ruthCourses.add( new Course(11656, "SPLN101", "Fund Of Speech I", "TBA", 04 ,3));
+		ruthCourses.add( new Course(11338, "BION111", "Human Biology Non-Majors", "Dadivinyan Ara", 05 ,4));
+		ruthCourses.add( new Course(11952, "COAN331", "Costume&Fashion in Art W Civ", "Terry-Seidenberg Rhoda", 05 ,3));
+		ruthCourses.add( new Course(12672, "EBAN102", "Prin Of Accounting II", "Halberstam Breindy", 05 ,3));
+		ruthCourses.add( new Course(12673, "EBAN314", "Fed Income Taxation Of Individ", "Bienenstock Shammai", 05 ,3));
+		ruthCourses.add( new Course(12676, "EBEN102", "Principles Of Microeconomics", "Szenberg Michael", 05 ,3));
+		ruthCourses.add( new Course(12678, "EBFN101", "Principles Of Finance", "Fischbein William", 05 ,3));
+		ruthCourses.add( new Course(12680, "EBKN101", "Principles Of Marketing", "Felder Simcha", 05 ,3));
+		ruthCourses.add( new Course(12681, "EBKN202", "Marketing Research", "Popkoff Eric", 05 ,3));
+		ruthCourses.add( new Course(10625, "EDUN201", "Psy/Soc Found Grwth-Dev&Lrn Bir-G6", "Dickstein Joel", 05 ,3));
+		ruthCourses.add( new Course(10619, "HISN220", "Survey Mod History I", "Singer Toba", 05 ,3));
+		ruthCourses.add( new Course(13578, "LLEN100", "Introduction to Eng Comp", "Maltzman Rachelle", 05 ,3));
+		ruthCourses.add( new Course(12757, "LLEN221", "Survey Of Modern Literature II", "Epstein Janey", 05 ,3));
+		ruthCourses.add( new Course(13391, "MATN111", "College Mathematics", "Siegel Stephanie", 05 ,3));
+		ruthCourses.add( new Course(11257, "MCON140", "Computer Concepts/Business Applctn", "Morgulis Debra", 05 ,3));
+		ruthCourses.add( new Course(11110, "POLN101", "American Politics", "Mond Alan", 05 ,3));
+		ruthCourses.add( new Course(13184, "PSYN201", "Developmental Psych", "Pretter Sheindy", 05 ,3));
+		ruthCourses.add( new Course(13181, "PSYN332", "History And System Of Psychology", "Steinman David", 05 ,3));
+		ruthCourses.add( new Course(13146, "PSYN345", "Psychology Of Health & Illness", "Litwin Elissa", 05 ,3));
+		ruthCourses.add( new Course(13150, "PSYN492", "Honors Seminar In Psychology", "Soffer Rebecca", 05 ,3));
+		ruthCourses.add( new Course(13579, "SASN493", "Topics: Technology and Society", "Ratti Deborah", 05 ,3));
+		ruthCourses.add( new Course(11657, "SPLN101", "Fund of Speech I", "TBA", 05 ,3));
+		ruthCourses.add( new Course(11947, "COAN101", "Art Of Western Civilization", "Terry-Seidenberg Rhoda", 06 ,3));
+		ruthCourses.add( new Course(12682, "EBAN101", "Prin Of Accounting I", "Halberstam Breindy", 06 ,3));
+		ruthCourses.add( new Course(12685, "EBAN201", "Intermediate Accounting I", "Saltz Simon", 06 ,3));
+		ruthCourses.add( new Course(12686, "EBAN302", "Government &Not-For-Profit Accoutng", "Newman Yosef", 06 ,3));
+		ruthCourses.add( new Course(12687, "EBAN316", "Corporation & Partnership Tax", "Bienenstock Shammai", 06 ,3));
+		ruthCourses.add( new Course(12689, "EBEN101", "Principles Of Macroeconomics", "Yarmish Morris", 06 ,3));
+		ruthCourses.add( new Course(12690, "EBKN101", "Principles Of Marketing", "Diamantstein Michael", 06 ,3));
+		ruthCourses.add( new Course(12691, "EBKN204", "Marketing Management", "Popkoff Eric", 06 ,3));
+		ruthCourses.add( new Course(10628, "EDUN311", "Principles Of Early Childhood Ed", "Vigilance Devika", 06 ,3));
+		ruthCourses.add( new Course(10621, "HISN220", "Survey Mod History I", "Singer Toba", 06 ,3));
+		ruthCourses.add( new Course(10622, "HISN251", "Jews And Arabs", "Nierman Jay Harris", 06 ,3));
+		ruthCourses.add( new Course(12758, "LLEN101", "English Composition I", "Maltzman Rachelle", 06 ,3));
+		ruthCourses.add( new Course(12760, "LLEN102", "English Composition II", "Epstein Janey", 06 ,3));
+		ruthCourses.add( new Course(12762, "LLEN219", "Lit of Ancient & Medieval Worlds", "Buchman Hedy", 06 ,3));
+		ruthCourses.add( new Course(13393, "MATN261", "Statistics For Soc Sci", "Siegel Stephanie", 06 ,3));
+		ruthCourses.add( new Course(11258, "MCON140", "Computer Concepts/Business Applctn", "Zlotnikov Aleksandr",06,3));
+		ruthCourses.add( new Course(11259, "MCON141", "Introduction To Programming", "Morgulis Debra", 06 ,3));
+//		ruthCourses.add( new Course(11256, "MCON343", "Data Base Concepts & Design", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+//		ruthCourses.add( new Course(, "", "", "", ,));
+
+	
 	}
 	
 	public ArrayList <Course> getRuthCourses() {
--- src/ScheduleUsingDB.java
+++ src/ScheduleUsingDB.java
@@ -1,3 +1,4 @@
+
 import java.awt.EventQueue;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -7,6 +8,9 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.*;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
@@ -25,6 +29,8 @@ public class ScheduleUsingDB extends JFrame {
 	private DefaultTableModel modelTable;
 	private RuthCourseObjects ruthCourses;
 	private JTextField userInputCourse;
+	private String [] tableHeadings = {"CRN", "Course Code", "Course Name", "Professor", "TimeSlot", "Credits"};
+
 
 
 	/**
@@ -65,6 +71,9 @@ public class ScheduleUsingDB extends JFrame {
 		panel = new JPanel();
 		tablePanel = new JPanel();
 		coursesTable = new JTable();
+		
+		//Do not adjust column widths automatically; use a horizontal scrollbar instead.
+		coursesTable.setAutoResizeMode( JTable.AUTO_RESIZE_OFF );		
 
 	}
 	/**
@@ -77,8 +86,7 @@ public class ScheduleUsingDB extends JFrame {
 		userInputCourse = new JTextField();
 
 		setupTable();
-		setGoButtonAction();
-
+		setActionListener();
 		setPanelLayout();
 
 		frame.getContentPane().add(panel);
@@ -97,76 +105,131 @@ public class ScheduleUsingDB extends JFrame {
 	private void setPanelLayout() {
 		frame.setBounds(200, 200, 650, 400);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-		go.setBounds(257, 66, 75, 29);
+		go.setBounds(272, 66, 75, 29);
 		enterCourse.setBounds(41, 65, 82, 29);
 		userInputCourse.setBounds(130, 66, 130, 26);
 
 	}
-	private void setGoButtonAction() {
+	
+	private void setActionListener() {
+		//listens out for an enter key
+		userInputCourse.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					theAction();
+
+				}
+			}
+		});
+
+		//listens out for a click
 		go.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				//all that happens inside:!
-				ArrayList <Course> courses = new ArrayList <Course> ();
-				ArrayList <Course> returnedCourses = new ArrayList <Course> ();
-				
-				//create connection
-				try {
-					
-					ConnectDB connectDB = new ConnectDB("jdbc:sqlserver://localhost;databaseName=CourseSchedule;integratedSecurity=true");
-					//query	
-					
-					InstantiateCourseObjects fillArray = new InstantiateCourseObjects(connectDB);
-					
-					//the course user is looking for
-					String inputCourse = userInputCourse.getText();
-
-					//get the arraylist of all courses
-					courses = fillArray.getCourses();
-					
-					//the arraylist that will hold the desired courses
-					
-					
-					//find all matching courses
-					returnedCourses = searchCourses(courses, inputCourse);
-					
-				}
-				catch(SQLException f)
-				{
-					System.out.println(f);
-				}
-				
-			
-				//if there are matching courses: 
-				if (courses.size() > 0) {
-
-					//Convert the containsCourse arraylist into an array. 
-					//This enables us to add the course info to the table
-					Course [] arrayOfCourses = new Course[returnedCourses.size()];
-
-					//populate the array 
-					for (int x = 0; x < returnedCourses.size(); x++) {
-						arrayOfCourses[x] = returnedCourses.get(x);
-					}
-
-					for (int x = 0; x < returnedCourses.size(); x++) {
-						Vector<Comparable> newRow = new Vector <Comparable>();
-						newRow.add(returnedCourses.get(x).getCRN());
-						newRow.add(returnedCourses.get(x).getCode());
-						newRow.add(returnedCourses.get(x).getTitle());
-						newRow.add(returnedCourses.get(x).getProfessor());
-						newRow.add(returnedCourses.get(x).getTimeSlot());
-						newRow.add(returnedCourses.get(x).getCredits());
-						modelTable.addRow(newRow); 
-					}
-					modelTable.fireTableDataChanged();
-
-				}
-
-
+				theAction();
 			}
 		});
-	}
+	}//end setActionListener
+
+	/**
+	 * Performs the meat of this class, finds desired courses and displays them nicely in a table
+	 */
+	private void theAction() {
+		ArrayList <Course> courses = new ArrayList <Course> ();
+		ArrayList <Course> returnedCourses = new ArrayList <Course> ();
+
+		//create connection
+		try {
+
+			ConnectDB connectDB = new ConnectDB("jdbc:sqlserver://localhost;databaseName=CourseSchedule;integratedSecurity=true");
+			//query	
+
+			InstantiateCourseObjects fillArray = new InstantiateCourseObjects(connectDB);
+
+			//the course user is looking for
+			String inputCourse = userInputCourse.getText();
+
+			//get the arraylist of all courses
+			courses = fillArray.getCourses();
+
+			//find all matching courses
+			returnedCourses = searchCourses(courses, inputCourse);
+
+		}
+		catch(SQLException f)
+		{
+			System.out.println(f);
+		}
+
+
+		//if there are matching courses: 
+		if (courses.size() > 0) {
+
+			//Convert the containsCourse arraylist into an array. 
+			//This enables us to add the course info to the table
+			Course [] arrayOfCourses = new Course[returnedCourses.size()];
+
+			//populate the array 
+			for (int x = 0; x < returnedCourses.size(); x++) {
+				arrayOfCourses[x] = returnedCourses.get(x);
+			}
+
+			for (int x = 0; x < returnedCourses.size(); x++) {
+				Vector<Comparable> newRow = new Vector <Comparable>();
+				newRow.add(returnedCourses.get(x).getCRN());
+				newRow.add(returnedCourses.get(x).getCode());
+				newRow.add(returnedCourses.get(x).getTitle());
+				newRow.add(returnedCourses.get(x).getProfessor());
+				newRow.add(returnedCourses.get(x).getTimeSlot());
+				newRow.add(returnedCourses.get(x).getCredits());
+				modelTable.addRow(newRow); 
+			}
+			modelTable.fireTableDataChanged();
+
+			//to resize the columns to fit the contents 
+			for (int x = 0; x < tableHeadings.length; x++) 
+				packColumn(coursesTable, x, 3);	
+		}
+	}//end theAction
+
+	//Credits to this method to Spot35 https://stackoverflow.com/questions/5820238/how-to-resize-jtable-column-to-string-length
+		/**
+		 * Sets the preferred width of the visible column specified by vColIndex. The column
+		 * will be just wide enough to show the column head and the widest cell in the column.
+		 * margin pixels are added to the left and right
+		 * (resulting in an additional width of 2*margin pixels).
+		 */ 
+		private void packColumn(JTable table, int vColIndex, int margin) {
+			DefaultTableColumnModel colModel = (DefaultTableColumnModel)table.getColumnModel();
+			TableColumn col = colModel.getColumn(vColIndex);
+			int width = 0;
+
+			// Get width of column header
+			TableCellRenderer renderer = col.getHeaderRenderer();
+			if (renderer == null) {
+				renderer = table.getTableHeader().getDefaultRenderer();
+			}
+			java.awt.Component comp = renderer.getTableCellRendererComponent(
+					table, col.getHeaderValue(), false, false, 0, 0);
+			width = comp.getPreferredSize().width;
+
+			// Get maximum width of column data
+			for (int r=0; r<table.getRowCount(); r++) {
+				renderer = table.getCellRenderer(r, vColIndex);
+				comp = renderer.getTableCellRendererComponent(
+						table, table.getValueAt(r, vColIndex), false, false, r, vColIndex);
+				width = Math.max(width, comp.getPreferredSize().width);
+			}
+
+			// Add margin
+			width += 2 * margin;
+
+			// Set the width
+			col.setPreferredWidth(width);
+		}
+
 	
 	public static ArrayList<Course> searchCourses(ArrayList<Course> courses, String courseCode)
 	{
@@ -189,11 +252,11 @@ public class ScheduleUsingDB extends JFrame {
 	 * Just the headings, no information inside
 	 */
 	private void setupTable() {
-		String [] tableHeadings = {"CRN", "Course Code", "Course Name", "Professor", "TimeSlot", "Credits"};
 		modelTable = new DefaultTableModel(tableHeadings, 0);	//to show outline of table but no information inside yet
 		tablePanel.setLayout(null);
 		coursesTable = new JTable(modelTable);
 		coursesTable.setEnabled(false);	//not to let users tamper with the output
+		
 		JScrollPane scrollPane = new JScrollPane(coursesTable);
 		scrollPane.setBounds(19, 156, 600, 190);
 		tablePanel.add(scrollPane);		//displays the top header
